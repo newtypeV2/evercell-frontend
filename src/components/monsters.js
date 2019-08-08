42894b75d9eb52e64d7d3ff28c9eeeb6e9e99fa9
newtypeV2 <gianlibunao@gmail.com>
2019-08-08 18:50:13 -0400

    Added some comments.

--- src/components/monsters.js
+++ src/components/monsters.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Monster = (props) => {
+    {/* !!Props to className mapping not FINAL!!! MONSTER WEAPON NEEDS TO BE REVISITED. */}
+    
     let weaponSide = props.monsterType.includes("mirror") ? "weapon_rusty_sword monster_ver left" : "weapon_rusty_sword monster_ver right"
     // let weaponSide = "weapon_knife right"
     return(
         <React.Fragment>
             {/* <span className={`${props.monsterType} monster`}></span> */}
+
+            {/* !!Props to className mapping not FINAL!!! MONSTER WEAPON NEEDS TO BE REVISITED. */}
+
             <span className={props.monsterType}></span>
             <span id="monsterWeapon" className={`monv ${weaponSide}`}></span>
         </React.Fragment>
