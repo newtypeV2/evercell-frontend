82ae1289af1c7d9853706a11bead9376d9e94e3d
newtypeV2 <gianlibunao@gmail.com>
2019-10-17 14:24:05 -0400

    updated constants.

--- src/constants.js
+++ src/constants.js
@@ -5,16 +5,20 @@
 // export const API = 'http://localhost:5000/'
 
     //localLAN
-export const API = `http://${window.location.hostname}:5000/`
+// export const API = `http://${window.location.hostname}:5000/`
+
 
     //deployment
-// export const API = ''
+export const API = 'https://evercell-gg.herokuapp.com/'
 
     //WS_URL localhost
 // export const WS_URL = 'ws://localhost:5000/cable'
 
     //ActionCable - flexible WS_URL
-export const WS_URL = `ws://${window.location.hostname}:5000/cable`
+// export const WS_URL = `ws://${window.location.hostname}:5000/cable`
+
+    //deployment
+export const WS_URL = 'ws://evercell-gg.herokuapp.com/cable'
 
 //AnyCable - port 3500
 // export const WS_URL = `ws://${window.location.hostname}:3500/cable`
